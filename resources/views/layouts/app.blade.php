3c62df1dde1ef05c809cdb43d6bcd9ecd3239a6f
Icaro Jobs <contato@wilby.com.br>
2018-06-21 18:52:31 +0000

    bugfix url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/jquery.dataTables.min.css">
@@ -23,7 +23,7 @@
 
     @include('includes.footer')
 
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ url('js/app.js') }}"></script>
     @include('includes.scripts.select2')
     @include('includes.scripts.sweetalert')
     @include('includes.scripts.datatables')
