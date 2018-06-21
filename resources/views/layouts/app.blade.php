2c82135e5195101ce186d2f6243b2a3ccdda5b6d
Icaro Jobs <contato@wilby.com.br>
2018-06-21 19:03:48 +0000

    bugfix fontawesome to shared hosting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
